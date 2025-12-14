93e3ad347b1e1e866426c43fff726d9138b64b5b
Sara Isabel del Sastre <chinchimilla@gmail.com>
2025-12-14 18:29:38 +0100

    Project Break terminado

---
+++ js/script2.js
@@ -0,0 +1,35 @@
+
+const imagenes = [
+    "../assets/img/ballena.jpg",
+    "../assets/img/baños.jpg",
+    "../assets/img/cabras.jpg",
+    "../assets/img/cebras.jpg",
+    "../assets/img/colibri.jpg",
+    "../assets/img/cuernos.jpg",
+    "../assets/img/cuervo.jpg",
+    "../assets/img/flamencos.jpg",
+    "../assets/img/fondo marino.jpg",
+    "../assets/img/gato.jpg",
+    "../assets/img/leon.jpg",
+    "../assets/img/lobo.jpg",
+    "../assets/img/mar.jpg",
+    "../assets/img/mono.jpg",
+    "../assets/img/nemo.jpg",
+    "../assets/img/oso polar.jpg",
+    "../assets/img/perro.jpg",
+    "../assets/img/rinoceronte.jpg",
+    "../assets/img/tiburones.jpg",
+    "../assets/img/tortuga agua.jpg",
+    "../assets/img/tortuga byn.jpg",
+];
+
+function imagenAleatoria(){
+    const aleatorio = Math.floor(Math.random()*imagenes.length);
+
+    const imagen = imagenes[aleatorio];
+    document.body.style.backgroundImage = `url('${imagen}')`;
+
+}
+
+imagenAleatoria();
+setInterval(imagenAleatoria, 10000);
